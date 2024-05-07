41abc51ec455fab6d067d162583145c76d20bdbf
abellinha11 <168592918+abellinha11@users.noreply.github.com>
2024-05-07 18:22:04 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def soma(a, b)
+  return a + b 
+
+if _name_== "main":
+   num1 = (float(inout("Digite o primeiro número: "))
+   num2 = (float(inout("Digite o segundo número: "))
+   rusultado = soma(num1, num2)
+  print(f"Resultado:{resultado}")
